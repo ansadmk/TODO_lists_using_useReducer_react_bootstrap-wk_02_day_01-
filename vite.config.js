7b7ce71f907358066d31e4782df51abf88158dcc
Mohemed ansad mk <92428126+ansadmk@users.noreply.github.com>
2023-06-12 12:12:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/TODO_lists_using_useReducer_react_bootstrap-wk_02_day_01-/'
 })
